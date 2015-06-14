7897b43718dab91ce568f97cb2bb96d5563c75fd
chibaofang <13474379185@163.com>
2015-06-14 17:24:00 +0800

    Update PRF5.r

--- PRF5.r
+++ PRF5.r
@@ -1,7 +1,31 @@
 library(TTR)
 library(forecast)
+
 setwd("D:/Program File/R")
 ls()
+
+plotForecastErrors <- function(forecasterrors)
+{
+  # make a histogram of the forecast errors:
+  mybinsize <- IQR(forecasterrors)/4
+  mysd   <- sd(forecasterrors)
+  mymin  <- min(forecasterrors) - mysd*5
+  mymax  <- max(forecasterrors) + mysd*3
+  # generate normally distributed data with mean 0 and standard deviation mysd
+  mynorm <- rnorm(10000, mean=0, sd=mysd)
+  mymin2 <- min(mynorm)
+  mymax2 <- max(mynorm)
+  if (mymin2 < mymin) { mymin <- mymin2 }
+  if (mymax2 > mymax) { mymax <- mymax2 }
+  # make a red histogram of the forecast errors, with the normally distributed data overlaid:
+  mybins <- seq(mymin, mymax, mybinsize)
+  hist(forecasterrors, col="red", freq=FALSE, breaks=mybins)
+  # freq=FALSE ensures the area under the histogram = 1
+  # generate normally distributed data with mean 0 and standard deviation mysd
+  myhist <- hist(mynorm, plot=FALSE, breaks=mybins)
+  # plot the normal curve as a blue line on top of the histogram of forecast errors:
+  points(myhist$mids, myhist$density, type="l", col="blue", lwd=2)
+}
 pur <- scan("pur.txt")
 
 plot.ts(pur)
@@ -10,3 +34,6 @@ pur_timeseries <- ts(log_pur, frequency=7, start=c(1,1))
 
 pur_forecasts <- HoltWinters(pur_timeseries)
 pur_forecasts2 <- forecast.HoltWinters(pur_forecasts, h=35)
+
+plot.forecast(pur_forecasts2)
+plotForecastErrors(pur_forecasts2$residuals)
